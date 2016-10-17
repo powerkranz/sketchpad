a1638438165f25873422048f2066142f2c608847
Holden <holden.blunschi@gmail.com>
2016-10-17 10:59:47 -0400

    work on createGrid()

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-    createGrid();
     $('#square').mouseenter(function() {
       $(this).css("background-color", "red");
     })
 })
 
 
- function createGrid(){
- var gridSize = prompt("Please enter a grid size.")
- for(i = 0; i < gridSize; i++){
-   $('#grid').append('#square');
- }
+
+function createGrid(){
+  var gridSize = prompt("Please enter a grid size.")
+  $square = $('#square')
+  for(i = 0; i < gridSize; i++) {
+    $('#grid').append('<td id="square"></td>');
+};
 }
