1e2fed073434ad8fb6d3b1a55fb220686ca8ed32
Holden Blunschi <holden.blunschi@gmail.com>
2016-10-16 20:08:28 -0400

    square color change functionality fully added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+    createGrid();
+    $('#square').mouseenter(function() {
+      $(this).css("background-color", "red");
+    })
 })
+
+
+ function createGrid(){
+ var gridSize = prompt("Please enter a grid size.")
+ for(i = 0; i < gridSize; i++){
+   $('#grid').append('#square');
+ }
+}
